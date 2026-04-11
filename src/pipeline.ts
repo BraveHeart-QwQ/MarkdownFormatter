5fd205d752da8f4330a82e8ec7e9c130f8527856
Hanie <holdonabraveheart_qwq@outlook.com>
2026-04-11 12:30:21 +0800

    feat: basic architecture.

---
+++ src/pipeline.ts
@@ -0,0 +1,12 @@
+//===----------------------------------------------------------------------===//
+//
+// @desc    : 组装 unified pipeline 的入口文件
+//
+//===----------------------------------------------------------------------===//
+
+// export function buildPipeline(config: FormatterConfig) {
+//     return unified()
+//         .use(remarkParse)
+//         .use(remarkGfm);
+//     // .use()
+// }
